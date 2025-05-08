afd21f23aa6ee00966985b49d7895109daae0f3c
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 11:20:54 -0300

    Create texto para python.py

---
+++ Python/Lista 5/texto para python.py
@@ -0,0 +1,9 @@
+x = 2                                     x = 2
+y = 5                                     y = 5
+se y > 8 então                            if y > 8:
+   y = y * 2                                 y = y * 2
+caso contrário,                           else:
+   x = x * 2                                 x = x * 2
+imprime (x + y)                           print (x + y)
+Resposta:9
+
