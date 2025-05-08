23f4d460aab1f7ffed69097094fbc72be23b6696
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 08:10:11 -0300

    Create Total de segundos.py

---
+++ Python/Lista 1/Total de segundos.py
@@ -0,0 +1,9 @@
+3) Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
+
+d = int(input('Dias: '))
+h = int(input('Horas: '))
+m = int(input('Minutos: '))
+s = int(input('Segundos: '))
+
+total = d*24*60*60 + h*60*60 + m*60 + s
+print (total)
