19bdd2b37fe588d4baafc4cc8aa5106bcaf21094
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 11:14:17 -0300

    Create Senha.py

---
+++ Python/Lista 3/Senha.py
@@ -0,0 +1,7 @@
+N = input("Nome de Usuário: ")
+S = input("Senha: ")
+while N == S:
+     print ("Senha não aceita")
+     N = input("Nome de Usuário: ")
+     S = input("Senha: ")
+     print ("Senha aceita")
