26cd39c924c493899a30dafe49e2823cab3dda9b
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 10:57:43 -0300

    Create DescontoMercadoria.py

---
+++ Python/Lista 1/DescontoMercadoria.py
@@ -0,0 +1,6 @@
+p = float(input('Preço: '))
+d = float(input('Desconto%: '))
+desconto = p * d / 100
+preço a pagar = p - desconto
+print (f'Desconto: R$ {desconto:.2f}')
+print(f'preço a pagar: R$ {novo:.2f}')
