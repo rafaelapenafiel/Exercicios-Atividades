dbbcdb5dfdbea78d7caa4c19ee546e376c84452c
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 11:21:38 -0300

    Create Quantos 'oi's.py

---
+++ Python/Lista 5/Quantos 'oi's.py
@@ -0,0 +1,8 @@
+
+                                   python:
+para i = 1 até 9                   n = 0
+   se i != 3, então                for i in range (1, 10):
+      para j = 1 até 6               if i != 3:
+         imprime 'oi'                   for j in range (1, 7):
+Resposta: 48                              n = n + 1
+                                   print (n)
