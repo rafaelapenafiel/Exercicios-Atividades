cd70e0047b55a4bd429990b7fc2101e98cd1c77b
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-08 11:10:01 -0300

    Create Maior dos números.py

---
+++ Python/Lista 2/Maior dos números.py
@@ -0,0 +1,9 @@
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+n3 = int(input("Número 3: "))
+if n1 > n2 and n1 > n3:
+    print (f' O maior número é {n1} ')
+elif n2 > n1 and n2 > n3:
+    print (f' O maior número é {n2} ')
+elif n3 > n1 and n3 > n2:
+    print (f' O maior número é {n3} ')
