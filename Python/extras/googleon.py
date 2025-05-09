ca16fcf301fd813b7a8f38af5a6b8b708669b580
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 07:36:03 -0300

    Create googleon.py

---
+++ Python/extras/googleon.py
@@ -0,0 +1,25 @@
+googleon = ['tnt', 'mlsm', 'zmnc']
+ordem = ' zmbtshjpnwlrcxkqvdgf'
+normal = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def troca (s):
+    resp = ''
+    for x in s:
+        resp = resp + normal [ordem.index (x)]
+    return resp
+
+
+
+n  = 'zmbzbm'
+ordem = 'zmbtshjpnwlrcxkqvdgf'
+ordem.index ('z')
+0
+ordem.index ('m')
+1
+ordem.index ('b')
+2
+0*20**0 + 1*20**1 + 2*20**2 + 0*20**3 + 1*20**4 + 2*20**5
+6560820
+set ('zmbzmb')
+{'b', 'z', 'm'}
+len (n) == len(set(n))
+False
