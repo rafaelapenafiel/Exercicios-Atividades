0367131076451d50fc01983c4bfca0fe347c57f8
Rafaela Penafiel Diniz <rafaela.diniz@fatec.sp.gov.br>
2025-05-09 07:21:17 -0300

    Create erros no código.py

---
+++ Python/extras/erros no código.py
@@ -0,0 +1,11 @@
+valor = float(input("Valor inicial: ")
+desconto = input("Porcentagem de desconto: ")
+valor = valor - valor * desconto/ 100
+print ("Novo valor: ", novo)
+
+
+valor = float(input("Valor inicial: "))
+desconto = float(input("Porcentagem de desconto: "))
+novo = valor - valor * desconto/ 100
+print ("Novo valor: ", novo)
+5
